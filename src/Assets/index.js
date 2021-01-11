41fd4d9f3e3502c5e98f7120d51a48ff66949038
rmodz <rcdev.games@gmail.com>
2021-01-11 15:49:43 +0700

    add loading animation

--- src/Assets/index.js
+++ src/Assets/index.js
@@ -2,6 +2,7 @@ export const Images = {
     logo: require('./Images/logo_transparent.png'),
     backspace: require('./Images/backspace.png'),
     backspaceOld: require('./Images/backspace_old.png'),
+    loading: require('./Images/loading.gif'),
 }
 
 export const Colors = {
--- src/Components/loading/index.js
+++ src/Components/loading/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View, ActivityIndicator, Text } from 'react-native';
+import { View, ActivityIndicator, Text, Image } from 'react-native';
 import globalStore from '../../Data/global';
 import { view } from '@risingstack/react-easy-state';
 import styles from './style';
+import { Images } from '../../Assets';
 
 const LoadingIndicator = () => {
     if(globalStore.isLoading) {
         return(
             <View style={styles.loading}>
                 <View style={styles.container}>
-                    <ActivityIndicator size='large' color='#FFF'/>
+                    <Image source={Images.loading} style={{
+                        height: 70,
+                        width: 90
+                    }}/>
                     <Text style={styles.textLoading}>Loading ...</Text>
                 </View>
             </View>
